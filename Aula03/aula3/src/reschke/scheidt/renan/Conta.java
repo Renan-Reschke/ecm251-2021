862422cb433187d873feae4ebeb677204a112d7e
Renan Reschke <renanreschke@hotmail.com>
2021-03-08 08:31:12 -0300

    Add visualizar saldo

--- Aula03/aula3/src/reschke/scheidt/renan/Conta.java
+++ Aula03/aula3/src/reschke/scheidt/renan/Conta.java
@@ -1,4 +1,25 @@
 package reschke.scheidt.renan;
 
 public class Conta {
+    //Atributos
+    String titular, cpf;
+    double saldo;
+    int numero;
+
+    //Métodos da classe
+    void depositar(){
+
+    }
+
+    void sacar(){
+
+    }
+
+    void transferirDinheiro(){
+
+    }
+
+    void visualizarSaldo(){
+        System.out.println("Saldo = R$" + this.saldo);
+    }
 }
--- Aula03/aula3/src/reschke/scheidt/renan/Main.java
+++ Aula03/aula3/src/reschke/scheidt/renan/Main.java
@@ -3,6 +3,8 @@ package reschke.scheidt.renan;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    //Cria e instancia um objeto do tipo conta
+        Conta c1 = new Conta();
+
     }
 }
