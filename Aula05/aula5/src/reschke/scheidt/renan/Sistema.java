fe6fd48e9d577e097edc25095044e7fe914fbf0b
Renan Reschke <renanreschke@hotmail.com>
2021-03-15 09:32:38 -0300

    Fim da aula

--- Aula05/aula5/src/reschke/scheidt/renan/Sistema.java
+++ Aula05/aula5/src/reschke/scheidt/renan/Sistema.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Sistema {
     private boolean executarSistema;
     private Scanner scanner;
+    private Conta conta;
 
     public void executar(){
         int opcao;
@@ -22,14 +23,38 @@ public class Sistema {
                 executarSistema = false;
                 break;
             case 1:
+                System.out.println("Saldo atual: R$" + this.conta.getSaldo());
                 break;
             case 2:
+                System.out.println("Qual quantia de dinheiro?");
+                double quantiaParaSacar = scanner.nextDouble();
+                if(this.conta.sacar(quantiaParaSacar)){
+                    System.out.println("Operação realizada com sucesso!");
+                }else{
+                    System.out.println("Não foi possível realizar a operação");
+                }
                 break;
             case 3:
-                break;
-            case 4:
+                System.out.println("Qual quantia de dinheiro?");
+                double quantiaParaDepositar = scanner.nextDouble();
+                this.conta.depositar(quantiaParaDepositar);
+                System.out.println("Operação realizada com sucesso!");
                 break;
             case 5:
+                System.out.println("Nome da conta:");
+                String nome = scanner.next();
+                System.out.println("Valor da conta:");
+                double valorDaConta = scanner.nextDouble();
+                System.out.println("Juros da conta:");
+                double jurosDaConta = scanner.nextDouble();
+                System.out.println("Informe a data de vencimento (ano-mes-dia)");
+                String dataVencimentoTitulo = scanner.next();
+                Titulo titulo = new Titulo(valorDaConta, nome, dataVencimentoTitulo, jurosDaConta);
+                if(this.conta.sacar(titulo.totalParaPagar())){
+                    System.out.println("Operação Realizada com Sucesso!");
+                } else {
+                    System.out.println("Algo de errado aconteceu!");
+                }
                 break;
             default:
                 System.out.println("Funcionalidade nao implementada");
@@ -39,6 +64,7 @@ public class Sistema {
     public Sistema() {
         this.scanner = new Scanner(System.in); //Deixa ler entrada do sistema
         this.executarSistema = true;
+        this.conta = new Conta("Mario", 1945, 1234);
     }
 
     private void exibirMenu() {
