8771fa04e09e7323adc5d88c0e9790313d386ce4
Renan Reschke <renanreschke@hotmail.com>
2021-03-18 09:16:51 -0300

    Finalização validação de cpf

--- Aula05-teo/src/reschke/scheidt/renan/Main.java
+++ Aula05-teo/src/reschke/scheidt/renan/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         String cpf = "529.982.247-25";
-        System.out.println("CPF valido: " + ValidarCPF.validarCpf(cpf));
+        System.out.println("CPF valido: " + ValidadorCPF.validarCpf(cpf));
     }
 }
