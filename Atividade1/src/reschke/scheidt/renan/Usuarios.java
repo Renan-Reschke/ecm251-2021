612664b6a5eb41ed6d4d56cfdbb89afdf03975ab
Renan Reschke <renanreschke@hotmail.com>
2021-03-22 08:38:12 -0300

    Criação do construtor para Usuarios

--- Atividade1/src/reschke/scheidt/renan/Usuarios.java
+++ Atividade1/src/reschke/scheidt/renan/Usuarios.java
@@ -1,7 +1,15 @@
 package reschke.scheidt.renan;
 
 public class Usuarios {
+    //Atributos
     private String nome;
     private String senha;
     private String email;
+
+    //Construtor
+    public Usuarios(String nome, String senha, String email){
+        this.nome = nome;
+        this.senha = senha;
+        this.email = email;
+    }
 }
