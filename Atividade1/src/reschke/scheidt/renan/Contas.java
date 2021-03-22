41191fbbf27cc798bd3a690f96759a8d15c7c46b
Renan Reschke <renanreschke@hotmail.com>
2021-03-22 09:00:38 -0300

    Criação do construtor para 'Contas' e teste do idConta

--- Atividade1/src/reschke/scheidt/renan/Contas.java
+++ Atividade1/src/reschke/scheidt/renan/Contas.java
@@ -1,6 +1,20 @@
 package reschke.scheidt.renan;
 
 public class Contas {
-    private static int idConta;
+    //Atributos
+    private int idConta;
+    private static int qntContas = 0;
     private double saldo;
+    private Usuarios usuario;
+
+    //Construtor
+    public Contas(Usuarios usuario){
+        this.usuario = usuario;
+        this.saldo = 0.00;
+        this.qntContas += 1;
+        this.idConta = qntContas;
+    }
+    public void verificarIdConta(){
+        System.out.println("id: "+ this.idConta);
+    }
 }
--- Atividade1/src/reschke/scheidt/renan/Main.java
+++ Atividade1/src/reschke/scheidt/renan/Main.java
@@ -4,6 +4,12 @@ package reschke.scheidt.renan;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Usuarios u1 = new Usuarios("u1","awffa", "wfafawf@awfgw");
+	Usuarios u2 = new Usuarios("u2", "awfafawf", "awaraw@hjbaw");
+	Contas c1 = new Contas(u1);
+	Contas c2 = new Contas(u2);
+	c2.verificarIdConta();
+	c1.verificarIdConta();
+
     }
 }
